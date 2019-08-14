42dbdac6273d8de7883b0b37c762c23a3d8de412
Samuel Terra <samuelterra22@hotmail.com>
2019-08-14 15:58:01 -0300

    Conserta rota de formulario de criar exercicio de membros inferiores

--- resources/views/layouts/lower-member/create.blade.php
+++ resources/views/layouts/lower-member/create.blade.php
@@ -16,7 +16,7 @@
         </div>
         <!-- /.box-header -->
         <!-- form start -->
-        <form action="{{route('lower-member')}}" method="post">
+        <form action="{{route('lower-member.create')}}" method="post">
             @csrf
             <div class="box-body">
                 @include('layouts.components.input_exercicio')
