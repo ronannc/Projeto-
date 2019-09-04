942a5d82e9633c1e6ed48d6bb58530b7bbaa6d32
Samuel Terra <samuelterra22@hotmail.com>
2019-09-04 14:21:33 -0300

    Adiciona mais objetivos

--- database/seeds/MethodTableSeeder.php
+++ database/seeds/MethodTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Models\Method;
+use Illuminate\Database\Seeder;
 
 class MethodTableSeeder extends Seeder
 {
@@ -13,37 +13,47 @@ class MethodTableSeeder extends Seeder
     public function run()
     {
         Method::create([
-            'name'    => 'Força',
+            'name'        => 'Força',
             'description' => 'Treino focado em força muscular',
         ]);
 
         Method::create([
-            'name'    => 'Hipertrofia',
+            'name'        => 'Hipertrofia',
             'description' => 'Treino focado em hipertrofia muscular',
         ]);
 
         Method::create([
-            'name'    => 'Resistência',
+            'name'        => 'Resistência',
             'description' => 'Treino focado em resistência muscular',
         ]);
 
         Method::create([
-            'name'    => 'Potência',
+            'name'        => 'Emagrecimento',
+            'description' => 'Treino focado em perda de gordura',
+        ]);
+
+        Method::create([
+            'name'        => 'Qualidade de vida',
+            'description' => 'Treino para qualidade de vida',
+        ]);
+
+        Method::create([
+            'name'        => 'Potência',
             'description' => 'Treino focado em potência muscular',
         ]);
 
         Method::create([
-            'name'    => 'Condicionamento Físico',
+            'name'        => 'Condicionamento Físico',
             'description' => 'Treino focado em condicionamento físico',
         ]);
 
         Method::create([
-            'name'    => 'Fadiga',
+            'name'        => 'Fadiga',
             'description' => 'Treino focado em fadiga',
         ]);
 
         Method::create([
-            'name'    => 'Oclusão Vascular',
+            'name'        => 'Oclusão Vascular',
             'description' => 'Treino focado em oclusão vascular',
         ]);
     }
