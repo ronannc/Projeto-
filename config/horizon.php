10e7d7aeaa8de1eed2edfabb1b446f394d1a457e
Samuel Terra <samuelterra22@hotmail.com>
2019-09-16 13:53:30 -0300

    Cria filas de notificações

--- config/horizon.php
+++ config/horizon.php
@@ -116,7 +116,10 @@ return [
         'production' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue'      => ['default'],
+                'queue'      => [
+                    'default',
+                    'notifications',
+                ],
                 'balance'    => 'simple',
                 'processes'  => 10,
                 'tries'      => 3,
@@ -126,7 +129,10 @@ return [
         'local' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue'      => ['default'],
+                'queue'      => [
+                    'default',
+                    'notifications',
+                ],
                 'balance'    => 'simple',
                 'processes'  => 3,
                 'tries'      => 3,
