0fe1118687e09d14b9bb95cd9c3ee66f0c5ae115
Túlio Parreira Cunha <tuliopcunha5@gmail.com>
2019-09-16 20:54:26 -0300

    Correção erro route:cache;

---
+++ app/Repositories/Contracts/MethodRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+interface MethodRepository
+{
+    public function save(array $data);
+
+    public function update(Model $model, array $data);
+
+    public function delete(Model $model);
+}
