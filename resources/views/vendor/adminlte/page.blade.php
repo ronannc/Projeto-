1e8e97ef30c163acdcc4f6539dae8a8d6822e7ed
ronan <ronannc1@yahoo.com.br>
2019-09-23 19:14:21 -0300

    Footer

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -126,6 +126,12 @@
         </div>
         <!-- /.content-wrapper -->
 
+        <footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Versão</b> 1.0.0
+            </div>
+            <strong>Copyright © 2019 Virtual Training.</strong> Todos direitos reservados.
+        </footer>
     </div>
     <!-- ./wrapper -->
 @stop
