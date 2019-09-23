f62f5cddf4e5922d47a9f44ef5d6e318a29b0a75
Túlio Parreira Cunha <tuliopcunha5@gmail.com>
2019-09-23 20:58:23 -0300

    Criação Rules e Authorize Avaliação fisica

--- app/Http/Requests/PhysicalAssessmentCreateRequest.php
+++ app/Http/Requests/PhysicalAssessmentCreateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PhysicalAssessmentCreateRequest extends FormRequest
@@ -13,7 +14,7 @@ class PhysicalAssessmentCreateRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return User::hasThisPermission('add_physical_assessment');
     }
 
     /**
@@ -24,7 +25,23 @@ class PhysicalAssessmentCreateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'neck' => 'integer|required|min:1|max:9999999',
+            'shoulder' => 'integer|required|min:1|max:9999999',
+            'chest' => 'integer|required|min:1|max:9999999',
+            'right_arm' => 'integer|required|min:1|max:9999999',
+            'left_arm' => 'integer|required|min:1|max:9999999',
+            'right_forearm' => 'integer|required|min:1|max:9999999',
+            'left_forearm' => 'integer|required|min:1|max:9999999',
+            'waist' => 'integer|required|min:1|max:9999999',
+            'abdomen' => 'integer|required|min:1|max:9999999',
+            'hip' => 'integer|required|min:1|max:9999999',
+            'right_thigh' => 'integer|required|min:1|max:9999999',
+            'left_thigh' => 'integer|required|min:1|max:9999999',
+            'right_calf' => 'integer|required|min:1|max:9999999',
+            'left_calf' => 'integer|required|min:1|max:9999999',
+            'height' => 'numeric|required|min:1|max:5',
+            'weight' => 'numeric|required|min:1|max:500',
+            'blood_pressure' => 'string|required'
         ];
     }
 }
