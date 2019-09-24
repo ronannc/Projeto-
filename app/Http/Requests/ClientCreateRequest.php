ec95b7a64c778df32c3aeb82c5d35f7b3298b8e6
Túlio Parreira Cunha <tuliopcunha5@gmail.com>
2019-09-23 21:22:54 -0300

    Criação Rules e Authorize Clientes

--- app/Http/Requests/ClientCreateRequest.php
+++ app/Http/Requests/ClientCreateRequest.php
@@ -24,7 +24,20 @@ class ClientCreateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'string|required|max:191',
+            'cpf' => 'string|required|max:191',
+            'sex' => 'string|required|max:1',
+            'blood_type' =>'required|in:A+,B+,O+,A-,B-,O-,AB+,AB-',
+            'phone' =>'required|string|max:191',
+            'birthday' => 'required|date',
+            'street' => 'required|string|max:191',
+            'neighborhood' => 'required|string|max:191',
+            'number' => 'required|integer|max:999999',
+            'complement' => 'required|string|max:191',
+            'zipcode' => 'required|string|max:20',
+            'city_id' => 'required|integer|exists:cities,id',
+            'company_id' => 'required|string|exists:companies,id',
+            'email' => 'required|unique:clients'
         ];
     }
 }
--- app/Http/Requests/ClientUpdateRequest.php
+++ app/Http/Requests/ClientUpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Client;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ClientUpdateRequest extends FormRequest
 {
@@ -23,8 +25,23 @@ class ClientUpdateRequest extends FormRequest
      */
     public function rules()
     {
+
+        $client = Client::find(request()->segment(2));
         return [
-            //
+            'name' => 'string|required|max:191',
+            'cpf' => 'string|required|max:191',
+            'sex' => 'string|required|max:1',
+            'blood_type' =>'required|in:A+,B+,O+,A-,B-,O-,AB+,AB-',
+            'phone' =>'required|string|max:191',
+            'birthday' => 'required|date',
+            'street' => 'required|string|max:191',
+            'neighborhood' => 'required|string|max:191',
+            'number' => 'required|integer|max:999999',
+            'complement' => 'required|string|max:191',
+            'zipcode' => 'required|string|max:20',
+            'city_id' => 'required|integer|exists:cities,id',
+            'company_id' => 'required|string|exists:companies,id',
+            'email' => ['required','email',Rule::unique('users')->ignore($client->id)]
         ];
     }
 }
