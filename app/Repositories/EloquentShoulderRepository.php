86fb777d00dd0df35cc4f2062c54c3f0781ea7ad
Samuel Terra <samuelterra22@hotmail.com>
2019-10-20 11:10:48 -0300

    Adiciona metodo de repositorio

--- app/Repositories/EloquentShoulderRepository.php
+++ app/Repositories/EloquentShoulderRepository.php
@@ -8,4 +8,8 @@ use App\Repositories\Contracts\ShoulderRepository;
 class EloquentShoulderRepository extends AbstractEloquentRepository implements ShoulderRepository
 {
     //
+    public function exerciseByClient($client_id)
+    {
+        // TODO: Implement exerciseByClient() method.
+    }
 }
