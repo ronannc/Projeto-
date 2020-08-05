8790b5b900b4d2558eac6ecc20537582b6b234ad
Ronan Nunes Campos <ronannc1@yahoo.com.br>
2020-08-04 21:48:09 -0300

    Update WorkoutCreateRequest.php
    removendo obrigatoriedade goal_id

--- app/Http/Requests/WorkoutCreateRequest.php
+++ app/Http/Requests/WorkoutCreateRequest.php
@@ -29,7 +29,6 @@ class WorkoutCreateRequest extends FormRequest
             'next_workout' => 'required|date',
             'interval' => 'required|string|max:191',
             'frequency' => 'required|string|max:191',
-            'goal_id' => 'required|string|exists:goals,id',
             'client_id' => 'required|string|exists:clients,id',
             'triceps' => 'required|array|min:1',
             'biceps'  => 'required|array|min:1',
