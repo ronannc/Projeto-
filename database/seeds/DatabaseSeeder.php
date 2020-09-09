d0074932091bd27e23274b35f3a1571b048265f4
Ronan <ronannc1@yahoo.com.br>
2020-09-09 20:32:19 -0300

    seed banco de dados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,17 +13,17 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(CompanyTableSeeder::class);
+//        $this->call(CompanyTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(WorkoutModeTableSeeder::class);
+//        $this->call(WorkoutModeTableSeeder::class);
         $this->call(BicepsTableSeeder::class);
         $this->call(TricepsTableSeeder::class);
         $this->call(ShouldersTableSeeder::class);
         $this->call(BackTableSeeder::class);
         $this->call(BreastsTableSeeder::class);
         $this->call(LowerMembersTableSeeder::class);
-        $this->call(ClientTableSeeder::class);
-        $this->call(GoalTableSeeder::class);
+//        $this->call(ClientTableSeeder::class);
+//        $this->call(GoalTableSeeder::class);
 //        $this->call(WorkoutTableSeeder::class);
 //        $this->call(PhysicalAssessmentsTableSeeder::class);
     }
